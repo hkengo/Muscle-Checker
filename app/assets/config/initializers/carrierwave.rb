2741a1f83575f773be7eab63a15a11431ca9b61a
“hkengo” <hayashikengojp@gmail.com>
2018-12-16 02:10:13 +0900

    fix carrierwave

--- app/assets/config/initializers/carrierwave.rb
+++ app/assets/config/initializers/carrierwave.rb
@@ -1,11 +1,12 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAIYCDNMAMW4DG2ELQ',
-    aws_secret_access_key: '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',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory = 'muscle-checker'
-  config.cache_storage = :fog
+  # config.fog_directory = 'muscle-checker'
+  # config.cache_storage = :fog
+  config.asset_host = 'https://s3.amazonaws.com/muscle-checker'
 end
