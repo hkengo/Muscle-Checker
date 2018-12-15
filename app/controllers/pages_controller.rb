bd392ef1c6105a480c076fe25d235390db035408
“hkengo” <hayashikengojp@gmail.com>
2018-12-16 04:48:23 +0900

    login 制御

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @training_record = current_user.training_records.new
     @training_record.part = 'push_up'
